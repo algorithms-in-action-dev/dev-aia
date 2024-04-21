c6da5a4075ef127360883124b6e3af1e7e821664
lee-naish <dr.lee.naish@gmail.com>
2024-04-21 10:59:33 +1000

    Dijkstra in good shape

--- src/algorithms/pseudocode/dijkstra.js
+++ src/algorithms/pseudocode/dijkstra.js
@@ -34,7 +34,7 @@ Shortest(G, s) //Given a graph G find a shortest path from start node s \\B 1
                         update Nodes, Parent etc with n & m \\Ref UpdateNodes
                     \\In}
                 \\In}
-                return \\B 5a
+                return \\B 99
                 \\Expl{ The shortest path to every node connected to s has been found.
                         If we were searching for an end node we have failed
                         and some indication of this should be returned.
@@ -54,7 +54,7 @@ Shortest(G, s) //Given a graph G find a shortest path from start node s \\B 1
 
 \\Code{
         Next_node
-            n <- RemoveMin(Nodes) // remove lowest cost element of Nodes PQ \\B 9
+            n <- PQRemoveMin(Nodes) // remove lowest cost element of Nodes PQ \\B 9
             \\Expl{ n is the node in the frontier with the shortest known path
                     back to s (if it has infinite cost it must be in a component
                     not connected to s; not actually in the frontier).
@@ -83,7 +83,7 @@ Shortest(G, s) //Given a graph G find a shortest path from start node s \\B 1
             \\Expl} 
             \\In{
                 Cost[m] <- Cost[n] + weight(n,m) // new cost is path length from s \\B 12
-                UpdateCost(Nodes, m, Cost[m]) // update cost in Nodes PQ \\B 13
+                PQUpdateCost(Nodes, m, Cost[m]) // update cost in Nodes PQ \\B 13
                 Parent[m] <- n \\B 14
                 \\Expl{ The shortest known path to m now goes via n.
                 \\Expl}
