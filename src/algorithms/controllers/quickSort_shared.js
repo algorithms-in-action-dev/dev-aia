b49ce63e33052720da0b180c875fd105a3e1e1cf
lee-naish <dr.lee.naish@gmail.com>
2024-06-29 18:01:52 +1000

    some QS bugs fixed

--- src/algorithms/controllers/quickSort_shared.js
+++ src/algorithms/controllers/quickSort_shared.js
@@ -380,7 +380,9 @@ export function run_QS(is_qs_median_of_3) {
           // assigning the pivot as the midpoint calculated above
           chunker_add_if(QS_BOOKMARKS.MEDIAN3_mid_to_middle_index, (vis, cur_mid, cur_left, cur_right) => {
             highlight(vis, cur_mid, false);
-            highlight(vis, cur_left, false);
+            // highlight seems to increment counter rather than set flag
+            if (cur_left !== cur_mid)
+              highlight(vis, cur_left, false);
             highlight(vis, cur_right, false);
           },
           [mid, left, right]); 
@@ -406,12 +408,14 @@ export function run_QS(is_qs_median_of_3) {
           swapAction(QS_BOOKMARKS.MEDIAN3_swap_A_idx_mid_with_A_idx_right_minus_1, mid, right-1);
 
           // pivot <- A[right - 1]
-          pivot_index = right-1
+          pivot_index = right-1;
           chunker_add_if(QS_BOOKMARKS.MEDIAN3_set_pivot_to_value_at_array_indx_right_minus_1, 
             (vis, cur_right, cur_left, cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot_index, cur_depth) => {
             unhighlight(vis, cur_right, false);
             unhighlight(vis, cur_right -1, false);
-            unhighlight(vis, cur_left, false);
+            // unhighlight seems to decrement counter rather than unset flag
+            if (cur_left !== cur_right -1)
+              unhighlight(vis, cur_left, false);
 
             refresh_stack(vis, cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot_index, cur_depth) // refresh stack to show pivot_index
           },
@@ -569,29 +573,26 @@ cur_finished_stack_frames, cur_i, cur_j, cur_pivot_index, cur_depth) // refresh 
       }
       // array of size 1, already sorted
       // has a conditional to specify which line it jumps to depending on the expanding and collapsing
-      // XXX not sure this is a good idea - recursion clearer in
-      // animation if final "// Done" is stopped at
-      // for each recursive call - user can mentally prepare for what we
-      // jump back to?
+      // BUGS plus not a great idea
+      // Modified so we stop at "// Done" for all base cases (could add
+      // other cases also XXX); code could be simplified.
       else if (left < a.length) {
-        finished_stack_frames.push(real_stack.pop());
-        let size_one_bookmark = isRecursionExpanded()
-          ? QS_BOOKMARKS.SHARED_quicksort_left_to_i_minus_1
-          : QS_BOOKMARKS.SHARED_skip_step;
+        // let size_one_bookmark = isRecursionExpanded()
+          // ? QS_BOOKMARKS.SHARED_quicksort_left_to_i_minus_1
+          // : QS_BOOKMARKS.SHARED_skip_step;
 
         chunker.add(
-          size_one_bookmark,
+          // size_one_bookmark,
+          QS_BOOKMARKS.SHARED_done_qs,
           (vis, l) => {
             vis.array.sorted(l);
           },
           [left],
         depth);
-      } else {
         finished_stack_frames.push(real_stack.pop());
-        chunker.add(QS_BOOKMARKS.SHARED_done_qs, refresh_stack, [
-          real_stack,
-          finished_stack_frames,
-        ], depth);
+      } else {
+        chunker.add(QS_BOOKMARKS.SHARED_done_qs);
+        finished_stack_frames.push(real_stack.pop());
       }
 
       return a; // Facilitates testing
