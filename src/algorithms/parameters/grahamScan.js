d97f5f64de7e45f6505831a34079165c9f17b952
lee-naish <dr.lee.naish@gmail.com>
2025-09-23 17:27:52 +1000

    New menu/URL etc stuff convex hull DC

--- src/algorithms/parameters/grahamScan.js
+++ src/algorithms/parameters/grahamScan.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+// XXX copied from DFSrec - may need more mods
 import React, { useState } from 'react';
 import MatrixParam from './helpers/MatrixParam';
 import '../../styles/Param.scss';
@@ -8,7 +9,7 @@ import { withAlgorithmParams, addURLGraph } from './helpers/urlHelpers'
 import { EXAMPLES } from './helpers/ErrorExampleStrings';
 
 const DEFAULT_SIZE = 5;
-const DFSrec = 'DFSrec\'s';
+const gwrap = 'grahamScan';
 const DEFAULT_START = 5; // XXX null should disable
 // const DEFAULT_END = null; // disable end nodes display/input
 // XXX For now we support at most one end node
@@ -20,17 +21,17 @@ const DEFAULT_HEUR = null;  // disable heuristic display/input
 const GRAPH_EGS = [ // XXX think up better examples?
         { name: 'Graph 1',
           size: 14,
-          coords: '4-3,2-7,7-11,9-3,12-6,13-2,12-16,17-2,20-4,34-4,26-9,30-6,34-10,38-5',
-          edges: '1-2-3,1-4-6,2-3-4,3-4-2,3-5-4,4-5-3,5-6-2,5-7-10,6-8-5,7-11-10,8-9-6,9-10-3,10-12-8,11-12-5,12-13-3,13-14-4'
+          coords: '33-10,2-7,11-7,14-11,22-7,19-4,21-15',
+          // coords: '4-3,2-7,7-11,9-3,12-6,13-2,12-16,17-2,20-4,34-4,26-9,30-6,34-10,38-5',
+          edges: ''
         },
         { name: 'Graph 2',
           size: 17,
           coords: '2-13,6-6,7-11,9-15,12-2,15-6,16-12,19-5,25-7,23-16,28-14,29-10,35-13,36-6,40-15, 39-2,42-10',
-          edges:
-'1-2-10,1-4-4,2-3-6,3-4-10,3-5-5,4-7-3,5-6-7,6-7-8,7-8-2,7-9,8-9-3,9-10-5,9-11-7, 10-11-7,11-13-4,12-13-8,12-14-6,13-14-7,13-15-7,14-16-6,15-16-2,15-17-5,16-17-2'
+          edges: ''
         }];
 
-function DFSrecParam({ mode, xyCoords, edgeWeights, size, start, end, heuristic, min, max }) {
+function gwrapParam({ mode, xyCoords, edgeWeights, size, start, end, heuristic, min, max }) {
   const [message, setMessage] = useState(null);
   let [start1, size1, graph_egs] =
          addURLGraph(GRAPH_EGS, xyCoords, edgeWeights, start, DEFAULT_START);
@@ -39,7 +40,7 @@ function DFSrecParam({ mode, xyCoords, edgeWeights, size, start, end, heuristic,
     <>
       {/* Matrix input */}
       <EuclideanMatrixParams
-        name="DFSrec"
+        name="grahamScan"
         mode="find"
         defaultSize={ size1 }
         defaultStart={ start1 }
@@ -49,7 +50,7 @@ function DFSrecParam({ mode, xyCoords, edgeWeights, size, start, end, heuristic,
         max={max || 49}
         graphEgs={graph_egs}
         symmetric
-        ALGORITHM_NAME={DFSrec}
+        ALGORITHM_NAME={gwrap}
         EXAMPLE={EXAMPLES.GEN_NUMBERS_BETWEEN_0_1}
         EXAMPLE2={EXAMPLES.GEN_SYMMETRIC_MATRIX}
         setMessage={setMessage} 
@@ -64,7 +65,7 @@ function DFSrecParam({ mode, xyCoords, edgeWeights, size, start, end, heuristic,
 
 
 // Define the prop types for URL Params
-DFSrecParam.propTypes = {
+gwrapParam.propTypes = {
   alg: PropTypes.string.isRequired,
   mode: PropTypes.string.isRequired,
   size: PropTypes.string.isRequired,
@@ -77,4 +78,4 @@ DFSrecParam.propTypes = {
   max: PropTypes.string.isRequired,
 };
 
-export default withAlgorithmParams(DFSrecParam); // Export with the wrapper for URL Params
+export default withAlgorithmParams(gwrapParam); // Export with the wrapper for URL Params
